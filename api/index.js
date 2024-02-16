3014fe8a7bbcec433ffddec53ee250d0752660e6
sieuphan131102 <ptsieu131102@gmail.com>
2024-02-16 16:13:09 +0700

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log("Server is running on port 3333");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}!`);
 });
