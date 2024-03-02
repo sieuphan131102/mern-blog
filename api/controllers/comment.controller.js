931ad2c3b3ad8ab2ab4fce43f8aa2095bec437cf
sieuphan131102 <ptsieu131102@gmail.com>
2024-03-02 22:34:53 +0700

    add delete functionality to the comment section

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -82,3 +82,22 @@ export const editComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    console.log(req.body);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not allowed to delete this comment")
+      );
+    }
+    await Comment.findByIdAndDelete(req.params.commentId);
+    res.status(200).json("Comment has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
